076a748ebd1fa68624a162fd884f85ce25f8ac81
unknown <timgabbasov@mail.ru>
2019-05-24 00:43:50 +0300

    add files

---
+++ contracts/crowdfundInstance.js
@@ -0,0 +1,87 @@
+import web3 from './web3';
+
+const address = '0x6d4daa97e24e77bc5911be4a6d0c58e08c6b89ea'; // Your deployed contract's address goes here
+
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "title",
+				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "durationInDays",
+				"type": "uint256"
+			},
+			{
+				"name": "amountToRaise",
+				"type": "uint256"
+			}
+		],
+		"name": "startProject",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "contractAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "projectStarter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "projectTitle",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "projectDesc",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "deadline",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "goalAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "ProjectStarted",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "returnAllProjects",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]; 
+
+const instance = new web3.eth.Contract(abi, address);
+
+export default instance;
